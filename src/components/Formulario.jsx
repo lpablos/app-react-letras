a7ba038e9e2dfaf289f80acbb243ac55e95e6129
Your Name <you@example.com>
2021-07-27 12:03:44 -0500

    add style in form component

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,8 +2,48 @@ import React from 'react'
 
 const Formulario = () => {
     return (
-        <div>
-            <h5>Formulario</h5>
+        <div className="bg-info">
+            <div className="container">
+                <div className="row">
+                    <form 
+                        action="" 
+                        method="post"
+                        className="col card text-white bg-transparent mb-5 mt-5 pb-2" 
+                        >
+                            <fieldset>
+                                <legend className="text-center">Buscador de letras de canciones</legend>
+                                <div className="row">
+                                    <div className="col-md-6">
+                                        <div className="form-group">
+                                            <label htmlFor="artista">Artista</label>
+                                            <input 
+                                                type="text" 
+                                                className="form-control"
+                                                placeholder="Nombre Artista"
+                                                name="artista"/>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <div className="form-group">
+                                            <label htmlFor="artista">Canción</label>
+                                            <input 
+                                                type="text" 
+                                                className="form-control"
+                                                placeholder="Nombre Canción"
+                                                name="cancion"/>
+                                        </div>
+                                    </div>
+                                </div>
+                                <button 
+                                    type="submit"
+                                    className="btn btn-primary float-right"
+                                    >
+                                    Buscar
+                                </button>
+                            </fieldset>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
